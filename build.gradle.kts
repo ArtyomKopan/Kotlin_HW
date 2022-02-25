5c06ae08bcbe4fea4de8039128936dc9164da2e2
ArtyomKopan <artem.kopan.2003@mail.ru>
2022-02-25 22:46:50 +0300

    I configured project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,25 @@
 plugins {
-kotlin("jvm") version "1.5.0"
-id("io.gitlab.arturbosch.detekt") version "1.17.0"
+    kotlin("jvm") version "1.5.0"
+    id("io.gitlab.arturbosch.detekt") version "1.17.0"
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
-mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-implementation(kotlin("stdlib"))
-detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.19.0")
-testImplementation(platform("org.junit:junit-bom:5.8.2"))
-testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation(kotlin("stdlib"))
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.19.0")
+    testImplementation(platform("org.junit:junit-bom:5.8.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
-useJUnitPlatform()
-testLogging {
-events("passed", "skipped", "failed")
-}
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
 }
