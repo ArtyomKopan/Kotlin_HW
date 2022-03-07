9373bc9cedb43c352696e89ffda84bc86e9bc988
ArtyomKopan <artem.kopan.2003@mail.ru>
2022-03-07 23:06:53 +0300

    I removed comments

--- src/test/kotlin/Homework1/EratosphenTest.kt
+++ src/test/kotlin/Homework1/EratosphenTest.kt
@@ -1,6 +1,4 @@
 import org.junit.jupiter.api.Assertions.assertEquals
-//import org.junit.jupiter.api.Test
-//import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
@@ -12,11 +10,6 @@ internal class EratosphenTest {
     fun `test primes`(expected: List<Int>, firstArg: Int) {
         assertEquals(expected, getPrimeNumbers(firstArg))
     }
-
-    /*@Test
-    fun `zero test`() {
-        assertThrows<IllegalArgumentException> { getPrimeNumbers(0) }
-    }*/
 
     companion object {
         @JvmStatic
