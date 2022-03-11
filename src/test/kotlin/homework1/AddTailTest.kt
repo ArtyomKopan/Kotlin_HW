1833073bd93e2fb43786dc495265d06b2861c67b
ArtyomKopan <artem.kopan.2003@mail.ru>
2022-03-12 02:01:43 +0300

    I removed unused import

--- src/test/kotlin/homework1/AddTailTest.kt
+++ src/test/kotlin/homework1/AddTailTest.kt
@@ -1,7 +1,7 @@
 package homework1
 
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 
 internal class AddTailTest {
     @Test
