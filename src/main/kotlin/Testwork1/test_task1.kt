015281b04c2fd819f43b1e3ff6d049d914d916fc
ArtyomKopan <artem.kopan.2003@mail.ru>
2022-03-28 14:53:22 +0300

    While tests and CLI not exist :(

---
+++ src/main/kotlin/Testwork1/test_task1.kt
@@ -0,0 +1,19 @@
+package testwork1
+
+import java.util.TreeSet
+
+class PriorityQueue<K : Comparable<K>, E>(Comparator: (Pair<K, E>, Pair<K, E>) -> Int) {
+    val queue = TreeSet<Pair<K, E>>(Comparator)
+
+    fun enqueue(element: E, priority: K) = queue.add(Pair(priority, element))
+
+    fun peek(): E = queue.first().second ?: throw IllegalArgumentException("Очередь пуста!")
+
+    fun remove() = queue.remove(queue.first())
+
+    fun roll(): E {
+        val maxElement = queue.first().second
+        queue.remove(queue.first())
+        return maxElement ?: throw IllegalArgumentException("Очередь пуста!")
+    }
+}
---
+++ src/test/kotlin/Testwork1/PriorityQueueTest.kt
@@ -0,0 +1,24 @@
+package Testwork1
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class PriorityQueueTest {
+
+    @Test
+    fun enqueue() {
+    }
+
+    @Test
+    fun peek() {
+    }
+
+    @Test
+    fun remove() {
+    }
+
+    @Test
+    fun roll() {
+    }
+}
