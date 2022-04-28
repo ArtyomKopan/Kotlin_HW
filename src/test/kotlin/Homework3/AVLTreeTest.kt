536006c62a31f3429180c1d44086112a0ef00d0a
ArtyomKopan <artem.kopan.2003@mail.ru>
2022-04-28 16:33:43 +0300

    I wrote parameterized tests

--- src/test/kotlin/Homework3/AVLTreeTest.kt
+++ src/test/kotlin/Homework3/AVLTreeTest.kt
@@ -2,208 +2,133 @@ package homework.three
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 internal class AVLTreeTest {
-    @Test
-    fun `put test when tree is empty`() {
+    @ParameterizedTest
+    @MethodSource("addPutTestInputData")
+    fun `put test`(expected: MutableSet<Int>, source: Map<Int, String>) {
         val tree = AVLTree<Int, String>()
-        tree[1] = "Jon Snow"
-        assertEquals(mutableSetOf(1), tree.keys)
+        source.forEach { tree[it.key] = it.value }
+        assertEquals(expected, tree.keys)
+    }
+
+    @ParameterizedTest
+    @MethodSource("addPutTestInputData")
+    fun `putAll test`(expected: MutableSet<Int>, source: Map<Int, String>) {
+        val tree = AVLTree<Int, String>()
+        tree.putAll(source)
+        assertEquals(expected, tree.keys)
+    }
+
+    @ParameterizedTest
+    @MethodSource("addGetTestInputData")
+    fun `get test`(expectedValue: String?, checkedKey: Int, source: Map<Int, String>) {
+        val tree = AVLTree<Int, String>()
+        tree.putAll(source)
+        assertEquals(expectedValue, tree[checkedKey])
+    }
+
+    @ParameterizedTest
+    @MethodSource("addContainsKeyTestInputData")
+    fun `contains key test`(expected: Boolean, checkedKey: Int, source: Map<Int, String>) {
+        val tree = AVLTree<Int, String>()
+        tree.putAll(source)
+        assertEquals(expected, tree.containsKey(checkedKey))
+    }
+
+    @ParameterizedTest
+    @MethodSource("addContainsValueTestInputData")
+    fun `contains value test`(expected: Boolean, checkedValue: String, source: Map<Int, String>) {
+        val tree = AVLTree<Int, String>()
+        tree.putAll(source)
+        assertEquals(expected, tree.containsValue(checkedValue))
+    }
+
+    @ParameterizedTest
+    @MethodSource("addGetTestInputData")
+    fun `remove test`(expectedValue: String?, removedKey: Int, source: Map<Int, String>) {
+        val tree = AVLTree<Int, String>()
+        tree.putAll(source)
+        assertEquals(expectedValue, tree.remove(removedKey))
+    }
+
+    @ParameterizedTest
+    @MethodSource("addReplaceTestInputData")
+    fun `replace test`(expectedValue: String?, key: Int, newValue: String, source: Map<Int, String>) {
+        val tree = AVLTree<Int, String>()
+        tree.putAll(source)
+        assertEquals(expectedValue, tree.replace(key, newValue))
     }
 
     @Test
-    fun `put test when key not in tree`() {
+    fun `clear test`() {
         val tree = AVLTree<Int, String>()
-        tree[1] = "Jon Snow"
-        tree[2] = "Tyrion Lannister"
-        tree[3] = "Arya Stark"
-        tree[4] = "Teon Grayjoy"
-        tree[5] = "Samvell Tarley"
-        assertEquals(mutableSetOf(1, 2, 3, 4, 5), tree.keys)
-    }
-
-    @Test
-    fun `put test when key in tree`() {
-        val tree = AVLTree<Int, String>()
-        tree[2] = "Tyrion Lannister"
-        tree[3] = "Arya Stark"
-        tree[4] = "Teon Grayjoy"
-        tree[5] = "Samvell Tarley"
-        tree[10] = "Jon Snow"
-        assertEquals(mutableSetOf(2, 3, 4, 5, 10), tree.keys)
-    }
-
-    @Test
-    fun `putAll test`() {
-        val tree = AVLTree<Int, String>()
-        val from = mapOf(
-            1 to "Jon Snow",
-            2 to "Tyrion Lannister",
-            3 to "Arya Stark",
-            4 to "Teon Grayjoy",
-            5 to "Samvell Tarley"
+        tree.putAll(
+            mapOf(
+                1 to "Jon Snow",
+                2 to "Tyrion Lannister",
+                3 to "Arya Stark",
+                4 to "Teon Grayjoy",
+                5 to "Samvell Tarley"
+            )
         )
-        tree.putAll(from)
-        assertEquals(mutableSetOf(1, 2, 3, 4, 5), tree.keys)
-    }
-
-    @Test
-    fun `remove test when tree is empty`() {
-        val tree = AVLTree<Int, String>()
-        val removedValue = tree.remove(6)
-        assertEquals(removedValue, null)
-    }
-
-    @Test
-    fun `remove test when key in tree - check value`() {
-        val tree = AVLTree<Int, String>()
-        val from = mapOf(
-            1 to "Jon Snow",
-            2 to "Tyrion Lannister",
-            3 to "Arya Stark",
-            4 to "Teon Grayjoy",
-            5 to "Samvell Tarley"
-        )
-        tree.putAll(from)
-        val removedValue = tree.remove(3)
-        assertEquals("Arya Stark", removedValue)
-    }
-
-    @Test
-    fun `remove test when key in tree - check keys`() {
-        val tree = AVLTree<Int, String>()
-        val from = mapOf(
-            1 to "Jon Snow",
-            2 to "Tyrion Lannister",
-            3 to "Arya Stark",
-            4 to "Teon Grayjoy",
-            5 to "Samvell Tarley"
-        )
-        tree.putAll(from)
-        tree.remove(4)
-        assertEquals(mutableSetOf(1, 2, 3, 5), tree.keys)
-    }
-
-    @Test
-    fun `contains key test when tree is empty`() {
-        val tree = AVLTree<Int, String>()
-        assertEquals(tree.containsKey(10), false)
-    }
-
-    @Test
-    fun `contains key test when key in tree`() {
-        val tree = AVLTree<Int, String>()
-        val from = mapOf(
-            1 to "Jon Snow",
-            2 to "Tyrion Lannister",
-            3 to "Arya Stark",
-            4 to "Teon Grayjoy",
-            5 to "Samvell Tarley"
-        )
-        tree.putAll(from)
-        assertEquals(tree.containsKey(2), true)
-    }
-
-    @Test
-    fun `contains key test when key not in tree`() {
-        val tree = AVLTree<Int, String>()
-        tree[1] = "Jon Snow"
-        tree[2] = "Tyrion Lannister"
-        assertEquals(tree.containsKey(3), false)
-    }
-
-    @Test
-    fun `contains value test when value in tree`() {
-        val tree = AVLTree<Int, String>()
-        val from = mapOf(
-            1 to "Jon Snow",
-            2 to "Tyrion Lannister",
-            3 to "Arya Stark",
-            4 to "Teon Grayjoy",
-            5 to "Samvell Tarley"
-        )
-        tree.putAll(from)
-        assertEquals(tree.containsValue("Jon Snow"), true)
-    }
-
-    @Test
-    fun `contains value test when value not in tree`() {
-        val tree = AVLTree<Int, String>()
-        val from = mapOf(
-            1 to "Jon Snow",
-            2 to "Tyrion Lannister",
-            3 to "Arya Stark",
-            4 to "Teon Grayjoy",
-            5 to "Samvell Tarley"
-        )
-        tree.putAll(from)
-        assertEquals(tree.containsValue("Melisandra"), false)
-    }
-
-    @Test
-    fun `get test when key in tree`() {
-        val tree = AVLTree<Int, String>()
-        val from = mapOf(
-            1 to "Jon Snow",
-            2 to "Tyrion Lannister",
-            3 to "Arya Stark",
-            4 to "Teon Grayjoy",
-            5 to "Samvell Tarley"
-        )
-        tree.putAll(from)
-        assertEquals("Jon Snow", tree[1])
-    }
-
-    @Test
-    fun `get test when key not in tree`() {
-        val tree = AVLTree<Int, String>()
-        val from = mapOf(
-            1 to "Jon Snow",
-            2 to "Tyrion Lannister",
-            3 to "Arya Stark",
-            4 to "Teon Grayjoy",
-            5 to "Samvell Tarley"
-        )
-        tree.putAll(from)
-        assertEquals(tree[6], null)
-    }
-
-    @Test
-    fun `replace test when key in tree`() {
-        val tree = AVLTree<Int, String>()
-        val from = mapOf(
-            1 to "Jon Snow",
-            2 to "Tyrion Lannister",
-            3 to "Arya Stark",
-        )
-        tree.putAll(from)
-        tree.replace(2, "Stannis Barateon")
-        assertEquals(mutableListOf("Jon Snow", "Stannis Barateon", "Arya Stark").toSet(), tree.values.toSet())
-    }
-
-    @Test
-    fun `replace test when key not in tree`() {
-        val tree = AVLTree<Int, String>()
-        val from = mapOf(
-            1 to "Jon Snow",
-            2 to "Tyrion Lannister",
-            3 to "Arya Stark",
-        )
-        tree.putAll(from)
-        tree.replace(6, "Stannis Barateon")
-        assertEquals(mutableListOf("Jon Snow", "Tyrion Lannister", "Arya Stark"), tree.values)
-    }
-
-    @Test
-    fun clear() {
-        val tree = AVLTree<Int, String>()
-        val from = mapOf(
-            1 to "Jon Snow",
-            2 to "Tyrion Lannister",
-            3 to "Arya Stark",
-        )
-        tree.putAll(from)
         tree.clear()
-        assertEquals(tree.isEmpty(), true)
+        assertEquals(true, tree.isEmpty())
+    }
+
+    companion object {
+        private val defaultMap = mapOf(
+            1 to "Jon Snow",
+            2 to "Tyrion Lannister",
+            3 to "Arya Stark",
+            4 to "Teon Grayjoy",
+            5 to "Samvell Tarley"
+        )
+
+        @JvmStatic
+        fun addPutTestInputData() = listOf(
+            Arguments.of(mutableSetOf(1), mapOf(1 to "Jon Snow")),
+            Arguments.of(
+                mutableSetOf(1, 2, 3, 4, 5), defaultMap
+            ),
+            Arguments.of(
+                mutableSetOf(1, 2, 3, 4), mapOf(
+                    1 to "Jon Snow",
+                    2 to "Tyrion Lannister",
+                    3 to "Arya Stark",
+                    4 to "Teon Grayjoy",
+                    1 to "Samvell Tarley"
+                )
+            )
+        )
+
+        @JvmStatic
+        fun addGetTestInputData() = listOf(
+            Arguments.of(null, 1, emptyMap<Int, String>()),
+            Arguments.of("Jon Snow", 1, defaultMap),
+            Arguments.of(null, 6, defaultMap)
+        )
+
+        @JvmStatic
+        fun addContainsKeyTestInputData() = listOf(
+            Arguments.of(false, 10, emptyMap<Int, String>()),
+            Arguments.of(true, 2, defaultMap),
+            Arguments.of(false, 10, defaultMap)
+        )
+
+        @JvmStatic
+        fun addContainsValueTestInputData() = listOf(
+            Arguments.of(true, "Jon Snow", defaultMap),
+            Arguments.of(false, "Melisandra", defaultMap)
+        )
+
+        @JvmStatic
+        fun addReplaceTestInputData() = listOf(
+            Arguments.of("Teon Grayjoy", 4, "Stannis Barateon", defaultMap),
+            Arguments.of(null, 6, "Dayeneris Targarien", defaultMap)
+        )
     }
 }
