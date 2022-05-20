11745a386c7bf6f84c09966dfebd4a77104000a0
ArtyomKopan <artem.kopan.2003@mail.ru>
2022-05-20 18:56:04 +0300

    I added interface and int class, vector realisation and tests

--- src/main/kotlin/Retest1/Vector.kt
+++ src/main/kotlin/Retest1/Vector.kt
@@ -1,22 +1,22 @@
 package retest.one
 
-class Vector<T : ArithmeticAvailable<T>>(val coordinates: List<ArithmeticAvailable<T>>) {
+class Vector<T : ArithmeticAvailable<T>>(val coordinates: List<T>) {
     private val dimension = coordinates.size
 
     operator fun plus(increment: Vector<T>): Vector<T> {
         require(this.dimension == increment.dimension) { "Длины векторов не совпадают!" }
-        return Vector((1 until this.dimension).map { coordinates[it] + increment.coordinates[it] }.toList())
+        return Vector((0 until this.dimension).map { coordinates[it] + increment.coordinates[it] }.toList())
     }
 
     operator fun minus(decrement: Vector<T>): Vector<T> {
         require(this.dimension == decrement.dimension) { "Длины векторов не совпадают!" }
-        return Vector((1 until this.dimension).map { this.coordinates[it] - decrement.coordinates[it] }.toList())
+        return Vector((0 until this.dimension).map { this.coordinates[it] - decrement.coordinates[it] }.toList())
     }
 
-    operator fun times(factor: Vector<T>): ArithmeticAvailable<T> {
+    operator fun times(factor: Vector<T>): T {
         require(this.dimension == factor.dimension) { "Длины векторов не совпадают!" }
-        var sum = this.coordinates[0] + factor.coordinates[0]
-        (1 until this.dimension).forEach { sum += ((this.coordinates[it] + factor.coordinates[it])) }
+        var sum = this.coordinates[0] * factor.coordinates[0]
+        (1 until this.dimension).forEach { sum += ((this.coordinates[it] * factor.coordinates[it])) }
         return sum
     }
 
